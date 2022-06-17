8d5f297736632819048c44bef40f377876de0480
nick-haoran <33177696+nick-haoran@users.noreply.github.com>
2022-06-17 23:45:22 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #(cd QQMusicApi && yarn start & cd ../) &
 (cd NeteaseCloudMusicApi && node app.js & cd ../) &
-nohup python3 bot.py & nohup python3 voice.py &
+nohup python3 bot.py &
