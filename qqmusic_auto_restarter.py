f1873526f09c42521892accf540d910f1a80a748
nick-haoran <33177696+nick-haoran@users.noreply.github.com>
2022-08-29 16:41:47 +0800

    auto restarter fix 2

--- qqmusic_auto_restarter.py
+++ qqmusic_auto_restarter.py
@@ -1,9 +1,9 @@
-from os import system
+from os import system,environ
 from json import loads
 with open("config.json", "r", encoding="utf-8") as f:
     configstr = f.read().replace('\\', '!')
     configtmp = loads(configstr)
     config = {k: v.replace('!', '\\') for k, v in configtmp.items()}
-    q_id=config['q_id']
+    environ['QQ'] = config['q_id']
 while True:
-    system(f"cd QQ* && QQ={q_id} npm start && cd ..")
+    system(r"cd QQ* && npm start && cd ..")
